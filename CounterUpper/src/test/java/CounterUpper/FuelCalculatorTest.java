479f568d087e74a2243287144a6acc22d04109c1
JakeTheGingScheuer <jacob.scheuer@accenture.com>
2019-12-04 14:51:06 -0500

    dividing by 3 rounds down

--- CounterUpper/src/test/java/CounterUpper/FuelCalculatorTest.java
+++ CounterUpper/src/test/java/CounterUpper/FuelCalculatorTest.java
@@ -7,13 +7,24 @@ public class FuelCalculatorTest {
     FuelCalculator testCalc;
 
     @Test
-    public void fuelCalculatorDividesMassBy3() {
+    public void fuelCalculatorShouldDivideMassBy3() {
+
         testCalc = new FuelCalculator();
         int fakeMass = 12;
         int actual = testCalc.divideMass(fakeMass);
         int expected = 4;
 
         assertEquals(expected, actual);
+    }
 
+    @Test
+    public void fuelCalculatorShouldRoundDownWhenDividingMass() {
+
+        testCalc = new FuelCalculator();
+        int fakeMass = 14;
+        int actual = testCalc.divideMass(fakeMass);
+        int expected = 4;
+
+        assertEquals(expected, actual);
     }
 }
